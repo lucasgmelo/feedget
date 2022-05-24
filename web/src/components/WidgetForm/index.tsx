64338b5b6ff26224f75c65af323b3c88a35d3eba
Lucas Melo <contatolucasgmelo@gmail.com>
2022-05-24 11:57:54 -0300

    :sauropod: create steps

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,11 +1,12 @@
-import { CloseButton } from "./CloseButton";
+import { CloseButton } from "../CloseButton";
 
 import bugImageUrl from "../assets/bug.svg";
 import ideaImageUrl from "../assets/idea.svg";
 import thoughtImageUrl from "../assets/thought.svg";
 import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: "Problema",
     image: {
@@ -29,7 +30,7 @@ const feedbackTypes = {
   },
 };
 
-type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>();
@@ -41,21 +42,7 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-          return (
-            <button
-              key={key}
-              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-              onClick={() => setFeedbackType(key as FeedbackType)}
-              type="button"
-            >
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          );
-        })}
-      </div>
+      <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
 
       <footer className="text-xs text-neutral-400">
         Feito com s2 pela rocketseat
