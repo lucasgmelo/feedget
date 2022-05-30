7995c089592a926f56455239bba29b0cf7b6ae19
Lucas Melo <contatolucasgmelo@gmail.com>
2022-05-30 17:33:18 -0300

    :sparkles: config nodemailer

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import nodemailer from 'nodemailer'
+
 import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7901705034c7c7",
+    pass: "d07c1cc290d8de"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot} = req.body;
@@ -14,6 +25,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oie@feedget.com>',
+    to: 'Lucas <lucasgmelo1203@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join('\n'),
   })
 
   return res.status(201).send({data: feedback})
