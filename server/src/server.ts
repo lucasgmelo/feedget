1eb585641e556f5bedb72022f337e6dd8d0f959e
Lucas Melo <contatolucasgmelo@gmail.com>
2022-05-30 17:22:27 -0300

    :sauropod: create feedback route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,24 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
+app.use(express.json());
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot} = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    }
+  })
+
+  return res.status(201).send({data: feedback})
+})
+
 app.listen(3333, () => {
-  console.log('server')
+  console.log('server is running babe')
 })
